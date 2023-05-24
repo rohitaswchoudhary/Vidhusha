e1fbf85d61f23f42f409df976aee1f9539bc74a4
rohitaswchoudhary <rohitaswchoudhary@gmail.com>
2023-05-24 18:22:49 +0530

    dfgs

--- Vidhusha/settings.py
+++ Vidhusha/settings.py
@@ -80,10 +80,22 @@ WSGI_APPLICATION = 'Vidhusha.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'URL': 'postgresql://postgres:gaMvdVs5tVDWOwsquns4@containers-us-west-106.railway.app:6424/railway',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'gaMvdVs5tVDWOwsquns4',
+        'HOST': 'containers-us-west-106.railway.app',
+        'PORT': 6424,
     }
 }
 
