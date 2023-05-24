e857b132557bb0704333828977209cf1203430c0
rohitaswchoudhary <rohitaswchoudhary@gmail.com>
2023-05-24 21:36:47 +0530

    t15

--- Vidhusha/settings.py
+++ Vidhusha/settings.py
@@ -138,14 +138,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- Vidhusha/wsgi.py
+++ Vidhusha/wsgi.py
@@ -9,10 +9,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 
+import sys
+path = '/home/rohitaswchoudhary/todowoo-project-drf'
+if path not in sys.path:
+    sys.path.insert(0, path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Vidhusha.settings'
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Vidhusha.settings')
-
 application = get_wsgi_application()
 
 app = application
